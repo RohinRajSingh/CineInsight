357595f815e730888b24102fb03b15b42b260374
Rohin Raj Singh <rohinrajsingh002@gmail.com>
2025-05-25 21:52:33 +0530

    i

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
-
-const API_KEY = "0e8587b3cd1b3ea23ac6cf7125f91b40";
-const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const getPopularMovies = async () => {
   const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
@@ -10,9 +9,7 @@ export const getPopularMovies = async () => {
 
 export const searchMovies = async (query) => {
   const response = await fetch(
-    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
-      query
-    )}`
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
   );
   const data = await response.json();
   return data.results;
